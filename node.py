19882a8f89080f0155a078af6d3d5a64f4a77794
N1ghtlyC0de <105951697+N1ghtlyC0de@users.noreply.github.com>
2023-05-02 20:02:05 -0500

    Delete node.py

--- node.py
+++
@@ -1,9 +0,0 @@
-#Se define la clase nodo que recibe los parametros de "data" que seria el precio y "serial" que vendria siendo el serial del producto
-
-class Node():
-  def __init__(self, serial=None, data=None):
-    self.data = data
-    self.serial = serial
-    self.prev = None
-    # self.tail = None
-    self.next = None
